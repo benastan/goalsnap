70fca1d18db27e8accdd08b2c26fed45d4b903c3
benastan <bennyjbergstein@gmail.com>
2012-11-24 18:40:00 -0500

    Established Tally/Goal/Reward relations.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :goal_rewards
+  has_many :tallies, :as => :axis
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,5 @@
 class Reward < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :goal_rewards
+  has_many :tallies, :as => :axis
 end
