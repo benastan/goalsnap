1a23c84d88690996ceb6c7c6af34b137b9623593
benastan <bennyjbergstein@gmail.com>
2012-11-24 18:40:00 -0500

    Established Tally/Goal/Reward relations.

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -4,4 +4,19 @@ describe Goal do
   it 'has many goal rewards' do
     should have_many :goal_rewards
   end
+
+  it 'has many tallies as axis' do
+    should have_many :tallies
+  end
+
+  context 'adding a tally to a goal' do
+    let (:goal) { FactoryGirl.create(:goal) }
+    let (:tally) { FactoryGirl.create(:tally) }
+
+    it 'sets the tally\'s axis to the goal' do
+      tally.axis = goal
+      tally.save
+      goal.tallies.include?(tally).should == true
+    end
+  end
 end
--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -4,4 +4,18 @@ describe Reward do
   it 'has many goal rewards' do
     should have_many :goal_rewards
   end
+
+  it 'has many tallies' do
+    should have_many :tallies
+  end
+
+  context 'adding a tally to a reward' do
+    let (:reward) { FactoryGirl.create(:reward) }
+    let (:tally) { FactoryGirl.create(:tally) }
+    it 'sets the tally\'s axis to the reward' do
+      tally.axis = reward
+      tally.save
+      reward.tallies.include?(tally).should == true
+    end
+  end
 end
