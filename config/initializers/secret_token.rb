f6fffdc1d85a37e3775fc90c3926d75bf6716de1
benastan <bennyjbergstein@gmail.com>
2012-11-24 13:36:55 -0500

    New rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goalsnap::Application.config.secret_token = '9acdadd908a589afa4aae3b78a068a862a23148967ec1fde52cced12744ff3a713540b679d8b31de38708372d4f95e05a27e441d127c6e535d641f06e884338c'
