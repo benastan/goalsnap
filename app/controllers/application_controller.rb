5d566034d564243ad47034d0cd26dd68e6efaf30
benastan <bennyjbergstein@gmail.com>
2012-11-25 14:50:27 -0500

    Added session helpers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) rescue nil
+  end
+  helper_method :current_user
+
+  def set_current_user user
+    session[:user_id] = user.nil? ? nil : user.id
+  end
+  helper_method :set_current_user
+
+  def must_be_logged_in
+    redirect_to login_path unless current_user
+  end
 end
