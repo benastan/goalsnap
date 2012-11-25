99910956f4ca14c95eeccd3c782b08fd099f14ea
benastan <bennyjbergstein@gmail.com>
2012-11-25 14:52:08 -0500

    Created login/logout actions + tests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by_email params[:email] rescue nil
+    if user
+      set_current_user user
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def delete
+    set_current_user nil
+    redirect_to root_path
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
 describe SessionsController do
+  let (:user) { FactoryGirl.create(:user) }
+
+  context 'user enters the wrong credententials' do
+    it 'redirects to the login form' do
+      post :create
+      should redirect_to login_path
+    end
+  end
+
+  context 'user enters credentials on the login page' do
+    it 'logs the user in' do
+      controller.should_receive(:set_current_user).with(user)
+      post :create, :email => user.email
+      should redirect_to root_path
+    end
+  end
+
+  context 'user navigates to log out page' do
+    it 'logs the user out' do
+      controller.should_receive(:set_current_user).with(nil)
+      post :delete
+      should redirect_to root_path
+    end
+  end
 end
