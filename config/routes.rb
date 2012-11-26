88d47c760d00c1333d37a5f5199e72d3f9ea4d3d
benastan <bennyjbergstein@gmail.com>
2012-11-25 22:28:23 -0500

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Goalsnap::Application.routes.draw do
   resources :tallies, :except => [ :edit, :delete, :index ]
 
-  resources :goal_rewards
+  resources :rewards, :only => [ :index, :create ]
 
-  resources :rewards
+  resources :goals, :only => [ :index, :create ]
+
   resources :users do
     resources :goal_rewards
   end
 
-  resources :goals
+  resources :goal_rewards
+
   get 'login' => 'sessions#new', :as => :login
   post 'login' => 'sessions#create', :as => :login
   get 'logout' => 'sessions#delete', :as => :logout
